a99f4c760c323d81a6ab4e9ff72b436b06700e16
Mati69lbt <mati69_lbt@hotmail.com>
2023-07-24 16:30:16 -0300

    act230724

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,13 @@
+import { Fragment, useContext } from "react";
+import { MARCAS, PLANES, YEARS } from "../constans";
+import CotizadorContext from "../context/CotizadorProvider";
+
 const Formulario = () => {
+  const { modal, setModal } = useContext(CotizadorContext);
+  console.log(modal);
   return (
     <>
+      <button onClick={() => setModal(true)}>Cambiar Modal de Context</button>
       <form action="">
         <div className="my-5">
           <label className="block mb-3 font-bold text-gray-400 uppercase">
@@ -11,8 +18,47 @@ const Formulario = () => {
             className="w-full p-3 bg-white border border-gray-200"
           >
             <option value="">-- Selecciona Marca</option>
+            {MARCAS.map((marca) => (
+              <option key={marca.id} value={marca.id}>
+                {marca.nombre}
+              </option>
+            ))}
           </select>
         </div>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            AÑO
+          </label>
+          <select
+            name="marca"
+            className="w-full p-3 bg-white border border-gray-200"
+          >
+            <option value="">-- Selecciona Marca</option>
+            {YEARS.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Elige un Plan
+          </label>
+          <div className="flex gap-3 item-center">
+            {PLANES.map((plan) => (
+              <Fragment key={plan.id}>
+                <label>{plan.nombre}</label>
+                <input type="radio" name="plan" value={plan.id} />
+              </Fragment>
+            ))}
+          </div>
+        </div>
+        <input
+          type="submit"
+          className="w-full bg-indigo-600 transition-colors text-white cursor-pointer p-3 uppercase font-bold "
+          value="Cotizar"
+        />
       </form>
     </>
   );
---
+++ src/context/CotizadorProvider.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+const CotizadorContext = createContext();
+
+const CotizadorProvider = ({ children }) => {
+  const [modal, setModal] = useState(false);
+  return (
+    <CotizadorContext.Provider value={{ modal, setModal }}>
+      {children}
+    </CotizadorContext.Provider>
+  );
+};
+
+export { CotizadorProvider };
+
+export default CotizadorContext;
